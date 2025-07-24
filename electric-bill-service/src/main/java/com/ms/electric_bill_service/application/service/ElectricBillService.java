c6ede3f545575f5f088b3f01260f129e891987f1
Veronica <vero_ely_vc@hotmail.com>
2025-07-23 23:54:02 -0500

    consume external api

--- electric-bill-service/src/main/java/com/ms/electric_bill_service/application/service/ElectricBillService.java
+++ electric-bill-service/src/main/java/com/ms/electric_bill_service/application/service/ElectricBillService.java
@@ -7,8 +7,6 @@ import com.ms.electric_bill_service.domain.exception.BillNotFoundException;
 import com.ms.electric_bill_service.domain.exception.ElectricBillServiceException;
 import org.springframework.stereotype.Service;
 
-import java.math.BigDecimal;
-
 @Service
 public class ElectricBillService implements ElectricBillServicePort {
     private final ElectricBillRepository electricBillRepository;
@@ -18,30 +16,14 @@ public class ElectricBillService implements ElectricBillServicePort {
         this.electricBillRepository = electricBillRepository;
     }
 
-    //@CircuitBreaker(name = "getBill", fallbackMethod = "getBillFallback")
     @Override
     public ElectricBill getBill(String providerId, String referenceNumber) {
         try {
             return electricBillRepository.findByReference(referenceNumber);
-        } catch (BillNotFoundException e) {
-            throw e;
-        } catch (ElectricBillServiceException e) {
+        } catch (BillNotFoundException | ElectricBillServiceException e) {
             throw e;
         } catch (Exception e) {
             throw new ElectricBillServiceException("Unexpected error processing bill", e);
         }
-    }
-
-    public ElectricBill getBillFallback(String referenceNumber, Exception e) {
-        if (e instanceof BillNotFoundException) {
-            return null; // O algún otro valor que indique que la factura no existe
-        }
-        return new ElectricBill(
-                referenceNumber,
-                BigDecimal.ZERO,
-                java.time.LocalDate.now(),
-                null,
-                null
-        );
     }
 }
