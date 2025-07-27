48dc8d60ac37e11d48cff2086625d4685191b5c1
Veronica <vero_ely_vc@hotmail.com>
2025-07-26 20:56:19 -0500

    update url external api

--- electric-bill-service/src/main/java/com/ms/electric_bill_service/infrastructure/adapter/feign/ElectricityProviderClient.java
+++ electric-bill-service/src/main/java/com/ms/electric_bill_service/infrastructure/adapter/feign/ElectricityProviderClient.java
@@ -3,11 +3,11 @@ package com.ms.electric_bill_service.infrastructure.adapter.feign;
 import com.ms.electric_bill_service.dto.ElectricBillResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "electricityProvider", url = "${external-services.electricity-provider.url}")
 public interface ElectricityProviderClient {
 
-    @GetMapping("/api/electricity/bill")
-    ElectricBillResponse getBill(@RequestParam("reference") String reference);
+    @GetMapping("/api/electricity/bill/{reference}")
+    ElectricBillResponse getBill(@PathVariable("reference") String reference);
 }
