db5b7f8317198236066e560907b55d85a4e70834
Veronica <vero_ely_vc@hotmail.com>
2025-08-02 20:47:03 -0500

    add microservice payment

---
+++ payment-management-service/src/main/java/com/ms/payment_management_service/PaymentManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ms.payment_management_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentManagementServiceApplication.class, args);
+	}
+
+}
