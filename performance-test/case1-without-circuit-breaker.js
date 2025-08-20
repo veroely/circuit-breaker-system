c9fada94bec8415a9cfb7ff3efb33b3bfe6a59d4
Veronica <vero_ely_vc@hotmail.com>
2025-08-20 15:13:33 -0500

    update test case

--- performance-test/case1-without-circuit-breaker.js
+++ performance-test/case1-without-circuit-breaker.js
@@ -1,18 +1,18 @@
 import http from "k6/http";
-import { check, sleep } from "k6";
+import { check } from "k6";
 import { buildHeaders, buildPayload } from "./common-request.js";
 
 export const options = {
   stages: [
-    { duration: '30s', target: 20 }, // Aumenta gradualmente a 20 usuarios virtuales en 30 segundos
-    { duration: '1m', target: 20 },  // Mantiene 20 usuarios virtuales durante 1 minuto
-    { duration: '10s', target: 0 },   // Disminuye a 0 usuarios
-  ],
-  thresholds: {
-    // Se puede configurar un umbral para que la prueba falle si el rendimiento se degrada demasiado.
-    'http_req_failed': ['rate<0.01'], // Falla si más del 1% de las peticiones fallan.
-    'http_req_duration': ['p(95)<200'], // Falla si el 95% de las peticiones tardan más de 200ms.
-  },
+    // Una rampa de subida agresiva para saturar los hilos rápidamente
+    { duration: '30s', target: 200 },
+
+    // Mantenemos la carga para observar el colapso total
+    { duration: '1m', target: 200 },
+
+    // Rampa de bajada
+    { duration: '15s', target: 0 },
+  ]
 };
 
 export default function () {
@@ -24,5 +24,4 @@ export default function () {
   check(res, {
     'status is 200': (r) => r.status === 200
   });
-  sleep(1);
 }
